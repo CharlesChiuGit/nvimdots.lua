bed4102f789f36add516b3c098d7c1f0b8037711
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-10-15 11:10:41 +0800

    chore(UI): make tint.nvim not so dark

--- lua/modules/ui/tint.lua
+++ lua/modules/ui/tint.lua
@@ -1,5 +1,5 @@
 require("tint").setup({
-	tint = -45, -- Darken colors, use a positive value to brighten
+	tint = -15, -- Darken colors, use a positive value to brighten
 	saturation = 0.6, -- Saturation to preserve
 	transforms = require("tint").transforms.SATURATE_TINT, -- Showing default behavior, but value here can be predefined set of transforms
 	tint_background_colors = true, -- Tint background portions of highlight groups
