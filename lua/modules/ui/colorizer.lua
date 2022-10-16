44fae73bc246ed7e9cf23673e2c398d6ebd42526
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-10-16 11:10:01 +0800

    chore(colorizer): disable it on `names`

--- lua/modules/ui/colorizer.lua
+++ lua/modules/ui/colorizer.lua
@@ -1,18 +1,16 @@
--- require("colorizer").setup({
--- 	filetypes = { "*" },
--- 	user_default_options = {
--- 		RGB = true, -- #RGB hex codes
--- 		RRGGBB = true, -- #RRGGBB hex codes
--- 		names = false, -- "Name" codes like Blue or blue
--- 		RRGGBBAA = true, -- #RRGGBBAA hex codes
--- 		rgb_fn = true, -- CSS rgb() and rgba() functions
--- 		hsl_fn = true, -- CSS hsl() and hsla() functions
--- 		css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
--- 		css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
--- 		-- Available modes: foreground, background
--- 		mode = "background", -- Set the display mode.
--- 		virtualtext = "■",
--- 	},
--- })
-
-require("colorizer").setup()
+require("colorizer").setup({
+	filetypes = { "*" },
+	user_default_options = {
+		RGB = true, -- #RGB hex codes
+		RRGGBB = true, -- #RRGGBB hex codes
+		names = false, -- "Name" codes like Blue or blue
+		RRGGBBAA = true, -- #RRGGBBAA hex codes
+		rgb_fn = true, -- CSS rgb() and rgba() functions
+		hsl_fn = true, -- CSS hsl() and hsla() functions
+		css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+		css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+		-- Available modes: foreground, background
+		mode = "background", -- Set the display mode.
+		virtualtext = "■",
+	},
+})
