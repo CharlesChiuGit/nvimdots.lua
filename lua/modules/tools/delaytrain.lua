045b60df4e514d7fc7ba2d4f289484be5e10b2e2
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-10-17 16:53:22 +0800

    feat(delaytrain): update config

--- lua/modules/tools/delaytrain.lua
+++ lua/modules/tools/delaytrain.lua
@@ -2,7 +2,7 @@ require("delaytrain").setup({
 	delay_ms = 1000, -- How long repeated usage of a key should be prevented
 	grace_period = 2, -- How many repeated keypresses are allowed
 	keys = { -- Which keys (in which modes) should be delayed
-		["nv"] = { "h", "j", "k", "l" },
-		["nvi"] = { "<Left>", "<Down>", "<Up>", "<Right>" },
+		["nv"] = { "h", "j", "k", "l", "<Left>", "<Down>", "<Up>", "<Right>" },
+		-- ["nvi"] = { "<Left>", "<Down>", "<Up>", "<Right>" },
 	},
 })
