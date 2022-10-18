be0ded7df8e7f2cc3edee050b93674f4a47f5b0c
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-10-18 15:29:07 +0800

    chore(vim-markdonw): change config to pure lua

--- lua/modules/lang/markdown.lua
+++ lua/modules/lang/markdown.lua
@@ -1,41 +1,30 @@
-vim.api.nvim_command([[
-" disable header folding
-let g:vim_markdown_folding_disabled = 1
+-- Disable header folding
+vim.g.vim_markdown_folding_disabled = true
 
-" do not use conceal feature, the implementation is not so good
-let g:vim_markdown_conceal = 1
-let g:vim_markdown_conceal_code_blocks = 0
+-- Enable markdown conceal
+vim.opt_local.conceallevel = 2
+vim.g.vim_markdown_conceal = true
+vim.g.vim_markdown_conceal_code_blocks = false
 
-" document title is expanded
-let g:vim_markdown_folding_level = 2
+-- Fenced languages syntax highlight
+vim.g.vim_markdown_fenced_languages = {
+	"c==cpp",
+	"viml=vim",
+	"bash=sh",
+	"ini=dosini",
+	"py=python",
+}
 
-" To enable conceal use Vim's standard conceal configuration, if vim_markdown_conceal is not set
-" setlocal conceallevel=2
+-- Highlight YAML/TOML/JSON front matter.
+vim.g.vim_markdown_frontmatter = true
+vim.g.vim_markdown_toml_frontmatter = true
+vim.g.vim_markdown_json_frontmatter = true
 
-" Disable math tex conceal and syntax highlight
-let g:tex_conceal = ""
-let g:vim_markdown_math = 0
+-- Strikethrough uses two tildes.
+vim.g.vim_markdown_strikethrough = true
 
-" use different name from filetype in code block
-let g:vim_markdown_fenced_languages = [
-      \ 'c++=cpp', 'viml=vim', 'bash=sh', 'ini=dosini', 'py=python'
-  \ ]
+-- Autosave before using `ge`.
+vim.g.vim_markdown_autowrite = true
 
-" Highlight YAML front matter as used by Jekyll or Hugo.
-let g:vim_markdown_frontmatter = 1
-
-" TOML syntax highlight.
-let g:vim_markdown_toml_frontmatter = 1
-
-" JSON syntax highlight.
-let g:vim_markdown_json_frontmatter = 1
-
-" Strikethrough uses two tildes.
-let g:vim_markdown_strikethrough = 1
-
-" Autosave before using `ge`.
-let g:vim_markdown_autowrite = 1
-
-" Let the TOC window autofit so that it doesn't take too much space
-let g:vim_markdown_toc_autofit = 1
-]])
+-- Let the TOC window autofit so that it doesn't take too much space
+vim.g.vim_markdown_toc_autofit = true
