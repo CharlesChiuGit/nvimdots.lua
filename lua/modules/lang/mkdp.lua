6edfc60f17889629836faa0795c484b67db18640
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-10-18 18:24:26 +0800

    chore(mkdp, vimtex): rewirte config in pure lua

--- lua/modules/lang/mkdp.lua
+++ lua/modules/lang/mkdp.lua
@@ -1,101 +1,81 @@
-vim.api.nvim_command([[
-" set to 1, nvim will open the preview window after entering the markdown buffer
-" default: 0
-let g:mkdp_auto_start = 1
+-- NOTE: Re-run the installation command for mkdp plugin, if you change any option.
+-- NOTE: Otherwise it will not take effect.
 
-" set to 1, the nvim will auto close current preview window when change
-" from markdown buffer to another buffer
-" default: 1
-let g:mkdp_auto_close = 1
+-- If true, open markdown preview after entering a markdown buffer.
+vim.g.mkdp_auto_start = true
 
-" set to 1, the vim will refresh markdown when save the buffer or
-" leave from insert mode, default 0 is auto refresh markdown as you edit or
-" move the cursor
-" default: 0
-let g:mkdp_refresh_slow = 0
+-- If true, close browser preview page when change from markdown buffer to another.
+vim.g.mkdp_auto_close = false
 
-" set to 1, the MarkdownPreview command can be use for all files,
-" by default it can be use in markdown file
-" default: 0
-let g:mkdp_command_for_global = 1
+-- If true, refresh preview when save the buffer or leave insert mode.
+-- If false, refresh preview as you edit or move the cursor.
+vim.g.mkdp_refresh_slow = true
 
-" set to 1, preview server available to others in your network
-" by default, the server listens on localhost (127.0.0.1)
-" default: 0
-let g:mkdp_open_to_the_world = 0
+-- If true, preview server will be available to others in the same network.
+-- Set it to true if using WSL.
+vim.g.mkdp_open_to_the_world = true
 
-" use custom IP to open preview page
-" useful when you work in remote vim and preview on local browser
-" more detail see: https://github.com/iamcco/markdown-preview.nvim/pull/9
-" default empty
-let g:mkdp_open_ip = ''
+-- Use custom IP to open preview page.
+-- Useful when you work in remote nvim and want to preview on local browser.
+-- Check https://github.com/iamcco/markdown-preview.nvim/pull/9 for more.
+vim.g.mkdp_open_ip = ""
 
-" specify browser to open preview page
-" for path with space
-" valid: `/path/with\ space/xxx`
-" invalid: `/path/with\\ space/xxx`
-" default: ''
-let g:mkdp_browser = ''
+-- Specify browser to open preview page.
+-- INFO: For path with space, use `/path/with\ space/xxx` instead of `/path/with\\ space/xxx`.
+vim.g.mkdp_browser = ""
 
-" set to 1, echo preview page url in command line when open preview page
-" default is 0
-let g:mkdp_echo_preview_url = 1
+-- If true, print preview page url in cmdline area.
+-- Useful when working on a remote server.
+vim.g.mkdp_echo_preview_url = true
 
-" a custom vim function name to open preview page
-" this function will receive url as param
-" default is empty
-let g:mkdp_browserfunc = ''
+-- Custom vim function to open preview page, which will receive an url as an input.
+vim.g.mkdp_browserfunc = ""
 
-" options for markdown render
-" mkit: markdown-it options for render
-" katex: katex options for math
-" uml: markdown-it-plantuml options
-" maid: mermaid options
-" disable_sync_scroll: if disable sync scroll, default 0
-" sync_scroll_type: 'middle', 'top' or 'relative', default value is 'middle'
-"   middle: mean the cursor position alway show at the middle of the preview page
-"   top: mean the vim top viewport alway show at the top of the preview page
-"   relative: mean the cursor position alway show at the relative positon of the preview page
-" hide_yaml_meta: if hide yaml metadata, default is 1
-" sequence_diagrams: js-sequence-diagrams options
-" content_editable: if enable content editable for preview page, default: v:false
-" disable_filename: if disable filename header for preview page, default: 0
-let g:mkdp_preview_options = {
-    \ 'mkit': {},
-    \ 'katex': {},
-    \ 'uml': {},
-    \ 'maid': {},
-    \ 'disable_sync_scroll': 0,
-    \ 'sync_scroll_type': 'middle',
-    \ 'hide_yaml_meta': 1,
-    \ 'sequence_diagrams': {},
-    \ 'flowchart_diagrams': {},
-    \ 'content_editable': v:false,
-    \ 'disable_filename': 0,
-    \ 'toc': {}
-    \ }
+-- options for markdown render
+-- mkit: markdown-it options for render
+-- katex: katex options for math
+-- uml: markdown-it-plantuml options
+-- maid: mermaid options
+-- disable_sync_scroll: if disable sync scroll, default 0
+-- sync_scroll_type: 'middle', 'top' or 'relative', default value is 'middle'
+--   middle: mean the cursor position alway show at the middle of the preview page
+--   top: mean the vim top viewport alway show at the top of the preview page
+--   relative: mean the cursor position alway show at the relative positon of the preview page
+-- hide_yaml_meta: if hide yaml metadata, default is 1
+-- sequence_diagrams: js-sequence-diagrams options
+-- content_editable: if enable content editable for preview page, default: v:false
+-- disable_filename: if disable filename header for preview page, default: 0
+vim.g.mkdp_preview_options = {
+	mkit = {},
+	katex = {},
+	uml = {},
+	maid = {},
+	disable_sync_scroll = false,
+	sync_scroll_type = "middle",
+	hide_yaml_meta = true,
+	sequence_diagrams = {},
+	flowchart_diagrams = {},
+	content_editable = false,
+	disable_filename = false,
+	toc = {},
+}
 
-" use a custom markdown style must be absolute path
-" like '/Users/username/markdown.css' or expand('~/markdown.css')
-let g:mkdp_markdown_css = ''
+-- Custom markdown style file, must be absolute path.
+-- e.g. `/home/USER/markdown.css` or `vim.fn.expand('~/markdown.css')`
+vim.g.mkdp_markdown_css = ""
 
-" use a custom highlight style must absolute path
-" like '/Users/username/highlight.css' or expand('~/highlight.css')
-" using https://github.com/sindresorhus/github-markdown-css
-let g:mkdp_highlight_css = fnameescape($HOME).'/.config/nvim/lua/github-markdown-css/github-markdown.css'
+-- Custom highlight style file, must be absolute path.
+-- Using https://github.com/sindresorhus/github-markdown-css
+vim.g.mkdp_highlight_css = os.getenv("HOME") .. "/.config/nvim/github-markdown-css/github-markdown.css"
 
-" use a custom port to start server or empty for random
-let g:mkdp_port = '6464'
+-- Custom port to start server. Leave it empty for random port.
+vim.g.mkdp_port = "6464"
 
-" preview page title
-" ${name} will be replace with the file name
-let g:mkdp_page_title = '「${name}」'
+-- Preview page title. ${name} is the filename.
+vim.g.mkdp_page_title = "「${name}」"
 
-" recognized filetypes
-" these filetypes will have MarkdownPreview... commands
-let g:mkdp_filetypes = ['markdown']
+-- Recognized filetypes. These filetypes will have `:MarkdownPreview` related commands.
+vim.g.mkdp_filetypes = { "markdown" }
 
-" set default theme (dark or light)
-" By default the theme is define according to the preferences of the system
-let g:mkdp_theme = 'dark'
-]])
+-- Default theme.
+vim.g.mkdp_theme = "dark"
