cd061d285a1e14ff401b77e52a3a02d4bdf89a18
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-10-20 14:33:38 +0800

    refactor(utils): move glowy function to utils/init.lua

--- lua/modules/ui/indent-blankline.lua
+++ lua/modules/ui/indent-blankline.lua
@@ -47,33 +47,11 @@ require("indent_blankline").setup({
 	space_char_blankline = " ",
 })
 
--- change indentline color for current scoop.
--- Below functions are from "folke/tokyonight.nvim/lua/tokyonight/util.lua" to creat the glowy feeling.
----@param c  string
-local function hexToRgb(c)
-	c = string.lower(c)
-	return { tonumber(c:sub(2, 3), 16), tonumber(c:sub(4, 5), 16), tonumber(c:sub(6, 7), 16) }
-end
-
----@param foreground string foreground color
----@param background string background color
----@param alpha number|string number between 0 and 1. 0 results in bg, 1 results in fg
-local function blend(foreground, background, alpha)
-	alpha = type(alpha) == "string" and (tonumber(alpha, 16) / 0xff) or alpha
-	local bg = hexToRgb(background)
-	local fg = hexToRgb(foreground)
-
-	local blendChannel = function(i)
-		local ret = (alpha * fg[i] + ((1 - alpha) * bg[i]))
-		return math.floor(math.min(math.max(0, ret), 255) + 0.5)
-	end
-
-	return string.format("#%02x%02x%02x", blendChannel(1), blendChannel(2), blendChannel(3))
-end
+local util = require("utils")
 
 -- Custom vertual text highlight groups for lsp_lines
 local c = require("kanagawa.colors").setup()
 local bg = require("kanagawa.colors").setup().bg
 local alpha = 0.3
-vim.api.nvim_set_hl(0, "IndentBlanklineContextChar", { fg = c.pp, bg = blend(c.pp, bg, alpha) })
+vim.api.nvim_set_hl(0, "IndentBlanklineContextChar", { fg = c.pp, bg = util.blend(c.pp, bg, alpha) })
 -- vim.api.nvim_set_hl(0, "IndentBlanklineContextChar", { fg = "#ec5241" })
