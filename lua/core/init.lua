f8df4d030b4c072c9694cd3b38db76f64d73c49a
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-10-20 14:34:48 +0800

    feat(core.init): make disable_distribution_plugins valid

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,4 +1,5 @@
 local global = require("core.global")
+local cmd = vim.api.nvim_command
 
 -- Create cache dir and data dirs
 local createdir = function()
@@ -23,48 +24,49 @@ end
 -- Disable few unused builtin plugins
 local disable_distribution_plugins = function()
 	-- Disable menu loading
-	vim.g.did_install_default_menus = 1
-	vim.g.did_install_syntax_menu = 1
+	cmd([[let g:did_install_default_menus = 1]])
+	cmd([[let g:did_install_syntax_menu = 1]])
 
 	-- Uncomment this if you define your own filetypes in `after/ftplugin`
-	-- vim.g.did_load_filetypes = 1
+	-- cmd([[let g:did_load_filetypes = 1]])
 
 	-- Do not load native syntax completion
-	vim.g.loaded_syntax_completion = 1
+	cmd([[let g:loaded_syntax_completion = 1]])
 
 	-- Do not load spell files
-	vim.g.loaded_spellfile_plugin = 1
+	cmd([[let g:loaded_spellfile_plugin = 1]])
 
 	-- Whether to load netrw by default
-	-- vim.g.loaded_netrw = 1
-	-- vim.g.loaded_netrwFileHandlers = 1
-	-- vim.g.loaded_netrwPlugin = 1
-	-- vim.g.loaded_netrwSettings = 1
+	-- cmd([[let g:loaded_netrw = 1]])
+	-- cmd([[let g:loaded_netrwFileHandlers = 1]])
+	-- cmd([[let g:loaded_netrwPlugin = 1]])
+	-- cmd([[let g:loaded_netrwSettings = 1]])
 	-- newtrw liststyle: https://medium.com/usevim/the-netrw-style-options-3ebe91d42456
-	vim.g.netrw_liststyle = 3
+	cmd([[let g:netrw_liststyle = 3]])
 
 	-- Do not load tohtml.vim
-	vim.g.loaded_2html_plugin = 1
+	cmd([[let g:loaded_2html_plugin = 1]])
 
 	-- Do not load zipPlugin.vim, gzip.vim and tarPlugin.vim (all these plugins are
 	-- related to checking files inside compressed files)
-	vim.g.loaded_gzip = 1
-	vim.g.loaded_tarPlugin = 1
-	vim.g.loaded_vimball = 1
-	vim.g.loaded_vimballPlugin = 1
-	vim.g.loaded_zipPlugin = 1
+	cmd([[let g:loaded_gzip = 1]])
+	cmd([[let g:loaded_tarPlugin = 1]])
+	cmd([[let g:loaded_vimball = 1]])
+	cmd([[let g:loaded_vimballPlugin = 1]])
+	cmd([[let g:loaded_zipPlugin = 1]])
 
 	-- Do not use builtin matchit.vim and matchparen.vim since the use of vim-matchup
-	vim.g.loaded_matchit = 1
-	vim.g.loaded_matchparen = 1
+	cmd([[let g:loaded_matchit = 1]])
+	cmd([[let g:loaded_matchparen = 1]])
 
 	-- Disable sql omni completion.
-	vim.g.loaded_sql_completion = 1
+	cmd([[let g:loaded_sql_completion = 1]])
 
 	-- Disable remote plugins
 	-- NOTE: Disabling rplugin.vim will show error for `wilder.nvim` in :checkhealth,
 	-- NOTE:  but since it's config doesn't require python rtp, it's fine to ignore.
 	-- vim.g.loaded_remote_plugins = 1
+	cmd([[let g:loaded_remote_plugins = 1]])
 end
 
 local leader_map = function()
