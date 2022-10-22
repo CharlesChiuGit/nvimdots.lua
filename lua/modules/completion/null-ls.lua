79b5806bc199e0a1b13ef1da21de2dd1eaffc330
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-10-22 10:53:45 +0800

    chore(ci): fix ci issue

--- lua/modules/completion/null-ls.lua
+++ lua/modules/completion/null-ls.lua
@@ -8,39 +8,39 @@ local diagnostics = null_ls.builtins.diagnostics
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-    debug = false,
-    sources = {
-        formatting.prettierd.with({
-            extra_filetypes = { "toml" },
-            extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-        }),
-        formatting.black.with({ extra_args = { "--fast" } }),
-        formatting.stylua,
-        formatting.shfmt,
-        formatting.isort,
-        formatting.markdownlint,
-        formatting.cbfmt,
-        diagnostics.eslint_d,
-        diagnostics.flake8,
-        diagnostics.shellcheck,
-        diagnostics.markdownlint.with({
-            extra_args = { "--disable MD033" },
-        }),
-        -- null_ls.builtins.code_actions.gitsigns, -- retrieve code actions from lewis6991/gitsigns.nvim, comment out to avoid code_actions lightball
-    },
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format()
-                    vim.notify("Formatted by null-ls!", vim.log.levels.INFO, { title = "LspFormat" })
-                end,
-            })
-        end
-    end,
+	debug = false,
+	sources = {
+		formatting.prettierd.with({
+			extra_filetypes = { "toml" },
+			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+		}),
+		formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.stylua,
+		formatting.shfmt,
+		formatting.isort,
+		formatting.markdownlint,
+		formatting.cbfmt,
+		diagnostics.eslint_d,
+		diagnostics.flake8,
+		diagnostics.shellcheck,
+		diagnostics.markdownlint.with({
+			extra_args = { "--disable MD033" },
+		}),
+		-- null_ls.builtins.code_actions.gitsigns, -- retrieve code actions from lewis6991/gitsigns.nvim, comment out to avoid code_actions lightball
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format()
+					vim.notify("Formatted by null-ls!", vim.log.levels.INFO, { title = "LspFormat" })
+				end,
+			})
+		end
+	end,
 })
 
 -- NOTE: if you want to use local executables.
--- lua/modules/completion/plugins.lua
+++ lua/modules/completion/plugins.lua
@@ -1,54 +1,54 @@
 local completion = {}
 
 completion["neovim/nvim-lspconfig"] = {
-    opt = true,
-    event = "BufReadPre",
-    after = "mason.nvim",
-    module = {
-        "lspconfig",
-        "lspconfig.util",
-    },
-    config = function()
-        require("modules.completion.lsp")
-    end,
+	opt = true,
+	event = "BufReadPre",
+	after = "mason.nvim",
+	module = {
+		"lspconfig",
+		"lspconfig.util",
+	},
+	config = function()
+		require("modules.completion.lsp")
+	end,
 }
 completion["williamboman/mason.nvim"] = {
-    opt = false,
-    -- event = "BufReadPre",
-    module = { "mason", "mason-registry" },
-    requires = {
-        {
-            "williamboman/mason-lspconfig.nvim",
-            module = "mason-lspconfig",
-        },
-    },
+	opt = false,
+	-- event = "BufReadPre",
+	module = { "mason", "mason-registry" },
+	requires = {
+		{
+			"williamboman/mason-lspconfig.nvim",
+			module = "mason-lspconfig",
+		},
+	},
 }
 completion["WhoIsSethDaniel/mason-tool-installer.nvim"] = {
-    opt = true,
-    after = "mason.nvim",
-    cmd = { "MasonToolsInstall", "MasonToolsUpdate" },
-    module = "mason-tool-installer",
-    config = function()
-        require("modules.completion.mason-install")
-    end,
+	opt = true,
+	after = "mason.nvim",
+	cmd = { "MasonToolsInstall", "MasonToolsUpdate" },
+	module = "mason-tool-installer",
+	config = function()
+		require("modules.completion.mason-install")
+	end,
 }
 completion["jose-elias-alvarez/null-ls.nvim"] = {
-    opt = true,
-    event = "BufReadPre",
-    config = function()
-        require("modules.completion.null-ls")
-    end,
+	opt = true,
+	event = "BufReadPre",
+	config = function()
+		require("modules.completion.null-ls")
+	end,
 }
 completion["glepnir/lspsaga.nvim"] = {
-    opt = true,
-    event = "LspAttach",
-    config = function()
-        require("modules.completion.saga")
-    end,
+	opt = true,
+	event = "LspAttach",
+	config = function()
+		require("modules.completion.saga")
+	end,
 }
 completion["ray-x/lsp_signature.nvim"] = {
-    opt = true,
-    after = "nvim-lspconfig",
+	opt = true,
+	after = "nvim-lspconfig",
 }
 -- completion["https://git.sr.ht/~whynothugo/lsp_lines.nvim"] = { -- not annoying disable for now
 -- 	opt = true,
@@ -58,72 +58,72 @@ completion["ray-x/lsp_signature.nvim"] = {
 -- 	end,
 -- }
 completion["hrsh7th/nvim-cmp"] = {
-    event = "BufReadPost",
-    after = { "LuaSnip" },
-    -- module = "cmp",
-    requires = {
-        { "onsails/lspkind.nvim", opt = true, module = "lspkind" },
-        { "lukas-reineke/cmp-under-comparator", opt = true, module = "cmp-under-comparator" },
-        { "saadparwaiz1/cmp_luasnip", after = { "nvim-cmp", "LuaSnip" } },
-        { "hrsh7th/cmp-nvim-lsp", after = "nvim-cmp", opt = true },
-        { "hrsh7th/cmp-nvim-lua", after = "nvim-cmp", opt = true },
-        { "andersevenrud/cmp-tmux", after = "nvim-cmp", opt = true },
-        { "hrsh7th/cmp-path", after = "nvim-cmp", opt = true },
-        { "hrsh7th/cmp-buffer", after = "nvim-cmp", opt = true },
-        { "kdheepak/cmp-latex-symbols", after = "nvim-cmp", opt = true },
-        { "ray-x/cmp-treesitter", after = "nvim-cmp", opt = true },
-        -- { "f3fora/cmp-spell", after = "cmp-path" },
-    },
-    config = function()
-        require("modules.completion.cmp")
-    end,
+	event = "BufReadPost",
+	after = { "LuaSnip" },
+	-- module = "cmp",
+	requires = {
+		{ "onsails/lspkind.nvim", opt = true, module = "lspkind" },
+		{ "lukas-reineke/cmp-under-comparator", opt = true, module = "cmp-under-comparator" },
+		{ "saadparwaiz1/cmp_luasnip", after = { "nvim-cmp", "LuaSnip" } },
+		{ "hrsh7th/cmp-nvim-lsp", after = "nvim-cmp", opt = true },
+		{ "hrsh7th/cmp-nvim-lua", after = "nvim-cmp", opt = true },
+		{ "andersevenrud/cmp-tmux", after = "nvim-cmp", opt = true },
+		{ "hrsh7th/cmp-path", after = "nvim-cmp", opt = true },
+		{ "hrsh7th/cmp-buffer", after = "nvim-cmp", opt = true },
+		{ "kdheepak/cmp-latex-symbols", after = "nvim-cmp", opt = true },
+		{ "ray-x/cmp-treesitter", after = "nvim-cmp", opt = true },
+		-- { "f3fora/cmp-spell", after = "cmp-path" },
+	},
+	config = function()
+		require("modules.completion.cmp")
+	end,
 }
 completion["L3MON4D3/LuaSnip"] = {
-    opt = true,
-    event = "BufReadPost",
-    module = {
-        "luasnip.loaders.from_vscode",
-        "luasnip.loaders.from_lua",
-        "luasnip.loaders.from_snipmate",
-    },
-    requires = { "rafamadriz/friendly-snippets", event = "InsertEnter" },
-    config = function()
-        require("modules.completion.luasnip")
-    end,
+	opt = true,
+	event = "BufReadPost",
+	module = {
+		"luasnip.loaders.from_vscode",
+		"luasnip.loaders.from_lua",
+		"luasnip.loaders.from_snipmate",
+	},
+	requires = { "rafamadriz/friendly-snippets", event = "InsertEnter" },
+	config = function()
+		require("modules.completion.luasnip")
+	end,
 }
 completion["windwp/nvim-autopairs"] = {
-    after = "nvim-cmp",
-    config = function()
-        require("modules.completion.autopairs")
-    end,
+	after = "nvim-cmp",
+	config = function()
+		require("modules.completion.autopairs")
+	end,
 }
 completion["zbirenbaum/copilot.lua"] = {
-    -- The options in cmp panel is still slow with VimEnter, so use BufReadPre for faster startup time.
-    -- event = "VimEnter",
-    event = "BufReadPre",
-    module = {
-        "copilot",
-        "copilot.util",
-    },
-    config = function()
-        vim.schedule(function()
-            require("modules.completion.copilot")
-        end)
-    end,
+	-- The options in cmp panel is still slow with VimEnter, so use BufReadPre for faster startup time.
+	-- event = "VimEnter",
+	event = "BufReadPre",
+	module = {
+		"copilot",
+		"copilot.util",
+	},
+	config = function()
+		vim.schedule(function()
+			require("modules.completion.copilot")
+		end)
+	end,
 }
 completion["zbirenbaum/copilot-cmp"] = {
-    after = "copilot.lua",
-    module = "copilot_cmp",
-    requires = "hrsh7th/nvim-cmp",
-    config = function()
-        require("modules.completion.copilot-cmp")
-    end,
+	after = "copilot.lua",
+	module = "copilot_cmp",
+	requires = "hrsh7th/nvim-cmp",
+	config = function()
+		require("modules.completion.copilot-cmp")
+	end,
 }
 
 completion["barreiroleo/ltex_extra.nvim"] = {
-    opt = true,
-    ft = "tex",
-    module = "ltex_extra",
+	opt = true,
+	ft = "tex",
+	module = "ltex_extra",
 }
 
 -- Adding *nvim config dir*, *nvim runtime dir*, *all plugin dir(with /lua dir)* to get
