f5182aba0cc2b2821cdf79f238af26c2207bf43d
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-10-23 09:09:14 +0800

    chore(null-ls): update null-ls config

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -3,7 +3,7 @@ local home = os.getenv("HOME")
 
 settings["use_ssh"] = true
 settings["format_disabled_dirs"] = {
-	home .. "/format_disabled_dir_under_home",
+	-- home .. "/format_disabled_dir_under_home",
 	home .. "/.config/alacritty",
 }
 
--- lua/modules/completion/null-ls.lua
+++ lua/modules/completion/null-ls.lua
@@ -1,4 +1,5 @@
 local null_ls = require("null-ls")
+local disabled_worksapces = require("core.settings").format_disabled_dirs
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
@@ -29,14 +30,26 @@ null_ls.setup({
 		-- null_ls.builtins.code_actions.gitsigns, -- retrieve code actions from lewis6991/gitsigns.nvim, comment out to avoid code_actions lightball
 	},
 	on_attach = function(client, bufnr)
-		if client.name == "null-ls" and client.supports_method("textDocument/formatting") then
+		local cwd = vim.fn.getcwd()
+		for i = 1, #disabled_worksapces do
+			if cwd.find(cwd, disabled_worksapces[i]) ~= nil then
+				return
+			end
+		end
+		if client.supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				group = augroup,
 				buffer = bufnr,
 				callback = function()
-					vim.lsp.buf.format()
-					vim.notify("Formatted by null-ls!", vim.log.levels.INFO, { title = "LspFormat" })
+					vim.lsp.buf.format({
+						name = "null-ls",
+					})
+					vim.notify(
+						string.format("Format successfully with %s!", client.name),
+						vim.log.levels.INFO,
+						{ title = "LspFormat" }
+					)
 				end,
 			})
 		end
