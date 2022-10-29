fe2595e052b6a14e66adb3e7fac751ebc2795ee0
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-10-29 21:22:07 +0800

    clean up

--- lua/core/event.lua
+++ lua/core/event.lua
@@ -1,14 +1,15 @@
 local autocmd = {}
+local cmd = vim.api.nvim_command
 
 function autocmd.nvim_create_augroups(definitions)
 	for group_name, definition in pairs(definitions) do
-		vim.api.nvim_command("augroup " .. group_name)
-		vim.api.nvim_command("autocmd!")
+		cmd("augroup " .. group_name)
+		cmd("autocmd!")
 		for _, def in ipairs(definition) do
 			local command = table.concat(vim.tbl_flatten({ "autocmd", def }), " ")
-			vim.api.nvim_command(command)
+			cmd(command)
 		end
-		vim.api.nvim_command("augroup END")
+		cmd("augroup END")
 	end
 end
 
