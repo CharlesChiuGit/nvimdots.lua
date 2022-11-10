f0a70c4f4e2b53fad34fe764cd074d22ad737b70
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-11-11 01:58:18 +0800

    chore(silicon): update config

--- lua/modules/tools/silicon.lua
+++ lua/modules/tools/silicon.lua
@@ -1,5 +1,7 @@
+local cmd = vim.api.nvim_command
+
 require("silicon").setup({
-	theme = "Dracula",
+	theme = "auto",
 	output = string.format(
 		"./SILICON_%s-%s-%s_%s-%s.png",
 		os.date("%Y"),
@@ -12,7 +14,7 @@ require("silicon").setup({
 	bgImage = "", -- path to image, must be png
 	roundCorner = true,
 	windowControls = true,
-	lineNumber = false,
+	lineNumber = true,
 	font = "monospace",
 	lineOffset = 1, -- from where to start line number
 	linePad = 2, -- padding between lines
@@ -20,9 +22,23 @@ require("silicon").setup({
 	padVert = 100, -- vertical padding
 	shadowBlurRadius = 10,
 	shadowColor = "#555555",
-	shadowOffsetX = 0,
-	shadowOffsetY = 0,
+	shadowOffsetX = 8,
+	shadowOffsetY = 8,
+	gobble = false, -- enable lsautogobble like feature(remove indentation)
+	debug = false, -- enable debug output
 })
+
+vim.api.nvim_create_augroup("SiliconRefresh", { clear = true })
+vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+	group = "SiliconRefresh",
+	callback = function()
+		require("silicon.utils").build_tmTheme()
+		require("silicon.utils").reload_silicon_cache({ async = true })
+	end,
+	desc = "Reload silicon themes cache on colorscheme switch",
+})
+
+cmd([[ command! Silicon execute 'lua require("silicon").visualise_cmdline({})' ]])
 
 -- NOTE: u have to install silicon first.
 -- NOTE: https://github.com/aloxaf/silicon
