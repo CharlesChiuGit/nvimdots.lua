599c3592a1205c0b45fb418569d545f2b20d6c32
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-11-23 05:53:53 +0800

    chore: try to fix s/S in leap

--- lua/modules/editor/leap.lua
+++ lua/modules/editor/leap.lua
@@ -6,4 +6,6 @@ vim.api.nvim_set_hl(0, "LeapMatch", {
 })
 require("leap").opts.highlight_unlabeled_phase_one_targets = true
 
+-- vim.keymap.set("n", "s", "<Nop>", { noremap = false })
+-- vim.keymap.set("n", "S", "<Nop>", { noremap = false })
 require("leap").add_default_mappings()
