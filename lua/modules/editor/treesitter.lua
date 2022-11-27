a81677322e8204282d0ee4cfd519bb592f265ab3
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-11-27 19:35:15 +0800

    chore(ts-rainbow): change `mac_file_lines` to 2000

--- lua/modules/editor/treesitter.lua
+++ lua/modules/editor/treesitter.lua
@@ -102,7 +102,7 @@ require("nvim-treesitter.configs").setup({
 	rainbow = {
 		enable = true,
 		extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-		max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+		max_file_lines = 2000, -- Do not enable for files with more than 2000 lines, int
 	},
 	context_commentstring = {
 		enable = true,
