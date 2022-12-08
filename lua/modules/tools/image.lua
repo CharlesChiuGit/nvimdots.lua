7bb9d2e073b6cace5837340248d5cad6ad63ec9a
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-12-08 13:26:37 +0800

    chore(image.nvim): add color support

--- lua/modules/tools/image.lua
+++ lua/modules/tools/image.lua
@@ -3,6 +3,8 @@ require("image").setup({
 		min_padding = 5,
 		show_label = true,
 		use_dither = true,
+		foreground_color = true,
+		background_color = true,
 	},
 	events = {
 		update_on_nvim_resize = true,
