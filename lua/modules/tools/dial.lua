e4731ac146d331de7f94b24c392715037d297c1f
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-12-10 12:28:22 +0800

    chore(dial): fix

--- lua/modules/tools/dial.lua
+++ lua/modules/tools/dial.lua
@@ -5,14 +5,12 @@ require("dial.config").augends:register_group({
 		augend.integer.alias.decimal, -- nonnegative decimal number (0, 1, 2, 3, ...)
 		augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
 		augend.date.new({
-			pattern = {
-				"%Y-%m-%d", -- date (2022-02-19, etc.)
-				"%Y/%m/%d",
-			},
+			pattern = "%Y-%m-%d", -- date (2022-02-19, etc.)
 			default_kind = "date",
 			only_valid = true,
 			word = false,
 		}),
+		augend.date.alias["%Y/%m/%d"],
 		augend.constant.alias.bool,
 		augend.constant.new({
 			elements = { "&&", "||" },
@@ -30,14 +28,12 @@ require("dial.config").augends:register_group({
 		augend.integer.alias.decimal,
 		augend.integer.alias.hex,
 		augend.date.new({
-			pattern = {
-				"%Y-%m-%d",
-				"%Y/%m/%d",
-			},
+			pattern = "%Y-%m-%d", -- date (2022-02-19, etc.)
 			default_kind = "date",
 			only_valid = true,
 			word = false,
 		}),
+		augend.date.alias["%Y/%m/%d"],
 
 		augend.constant.alias.alpha, -- Lowercase alphabet letter (word)
 		augend.constant.alias.Alpha, -- Uppercase alphabet letter (word)
