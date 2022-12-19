d25e6df4c7c5ee52a42af866e97a4c61f67500f8
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-12-19 18:25:01 +0800

    chore(lsp): update clangd offsetEncodeing to utf-16 & utf-8

--- lua/modules/completion/lsp.lua
+++ lua/modules/completion/lsp.lua
@@ -66,38 +66,34 @@ local opts = {
 for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
 	if server == "bashls" then
 		local bashls_opts = require("modules.completion.server-settings.bashls")
-		local extra_opts = vim.tbl_deep_extend("force", bashls_opts, opts)
+		local extra_opts = vim.tbl_deep_extend("keep", bashls_opts, opts)
 		lspconfig.bashls.setup(extra_opts)
 	end
 
 	if server == "clangd" then
 		local clangd_capabilities = capabilities
-		-- Workaround for clangd warning: multiple different client offset_encodings detected for buffer, this is not supported yet
-		clangd_capabilities.offsetEncoding = { "utf-16" }
+		clangd_capabilities.offsetEncoding = { "utf-16", "utf-8" }
 		local clangd_opts = require("modules.completion.server-settings.clangd")
-		local extra_opts = vim.tbl_deep_extend(
-			"force",
-			clangd_opts,
-			{ on_attach = opts.on_attach, capabilities = clangd_capabilities }
-		)
+		local extra_opts =
+			vim.tbl_deep_extend("keep", clangd_opts, { on_attach = opts.on_attach, capabilities = clangd_capabilities })
 		lspconfig.clangd.setup(extra_opts)
 	end
 
 	if server == "emmet_ls" then
 		local emmetls_opts = require("modules.completion.server-settings.emmet_ls")
-		local extra_opts = vim.tbl_deep_extend("force", emmetls_opts, opts)
+		local extra_opts = vim.tbl_deep_extend("keep", emmetls_opts, opts)
 		lspconfig.emmet_ls.setup(extra_opts)
 	end
 
 	if server == "gopls" then
 		local gopls_opts = require("modules.completion.server-settings.gopls")
-		local extra_opts = vim.tbl_deep_extend("force", gopls_opts, opts)
+		local extra_opts = vim.tbl_deep_extend("keep", gopls_opts, opts)
 		lspconfig.gopls.setup(extra_opts)
 	end
 
 	if server == "jsonls" then
 		local jsonls_opts = require("modules.completion.server-settings.jsonls")
-		local extra_opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+		local extra_opts = vim.tbl_deep_extend("keep", jsonls_opts, opts)
 		lspconfig.jsonls.setup(extra_opts)
 	end
 
@@ -121,51 +117,51 @@ for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
 		end
 		local ltex_opts = require("modules.completion.server-settings.ltex")
 		local extra_opts =
-			vim.tbl_deep_extend("force", ltex_opts, { on_attach = ltex_attach, capabilities = opts.capabilities })
+			vim.tbl_deep_extend("keep", ltex_opts, { on_attach = ltex_attach, capabilities = opts.capabilities })
 		lspconfig.ltex.setup(extra_opts)
 	end
 
 	if server == "marksman" then
 		local marksman_opts = require("modules.completion.server-settings.marksman")
-		local extra_opts = vim.tbl_deep_extend("force", marksman_opts, opts)
+		local extra_opts = vim.tbl_deep_extend("keep", marksman_opts, opts)
 		lspconfig.marksman.setup(extra_opts)
 	end
 
 	if server == "pyright" then
 		local pyright_opts = require("modules.completion.server-settings.pyright")
-		local extra_opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+		local extra_opts = vim.tbl_deep_extend("keep", pyright_opts, opts)
 		lspconfig.pyright.setup(extra_opts)
 	end
 
 	if server == "sumneko_lua" then
 		-- require("lua-dev").setup()
 		local sumneko_opts = require("modules.completion.server-settings.sumneko_lua")
-		local extra_opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+		local extra_opts = vim.tbl_deep_extend("keep", sumneko_opts, opts)
 		-- print(vim.inspect(extra_opts))
 		lspconfig.sumneko_lua.setup(extra_opts)
 	end
 
 	if server == "taplo" then
 		local taplo_opts = require("modules.completion.server-settings.taplo")
-		local extra_opts = vim.tbl_deep_extend("force", taplo_opts, opts)
+		local extra_opts = vim.tbl_deep_extend("keep", taplo_opts, opts)
 		lspconfig.taplo.setup(extra_opts)
 	end
 
 	if server == "texlab" then
 		local texlab_opts = require("modules.completion.server-settings.texlab")
-		local extra_opts = vim.tbl_deep_extend("force", texlab_opts, opts)
+		local extra_opts = vim.tbl_deep_extend("keep", texlab_opts, opts)
 		lspconfig.texlab.setup(extra_opts)
 	end
 
 	if server == "vimls" then
 		local vimls_opts = require("modules.completion.server-settings.vimls")
-		local extra_opts = vim.tbl_deep_extend("force", vimls_opts, opts)
+		local extra_opts = vim.tbl_deep_extend("keep", vimls_opts, opts)
 		lspconfig.vimls.setup(extra_opts)
 	end
 
 	if server == "yamlls" then
 		local yamlls_opts = require("modules.completion.server-settings.yamlls")
-		local extra_opts = vim.tbl_deep_extend("force", yamlls_opts, opts)
+		local extra_opts = vim.tbl_deep_extend("keep", yamlls_opts, opts)
 		lspconfig.yamlls.setup(extra_opts)
 	end
 end
