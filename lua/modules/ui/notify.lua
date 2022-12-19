5ff9cfa29443a491dff288099a20cc2a7dc893ee
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-12-19 10:36:06 +0800

    chore(notify): Update options

--- lua/modules/ui/notify.lua
+++ lua/modules/ui/notify.lua
@@ -5,32 +5,23 @@ local icons = {
 }
 
 require("notify").setup({
-	-- Animation style (see below for details)
+	---@usage Animation style one of { "fade", "slide", "fade_in_slide_out", "static" }
 	stages = "slide",
-
-	-- Function called when a new window is opened, use for changing win settings/config
+	---@usage Function called when a new window is opened, use for changing win settings/config
 	on_open = nil,
-
-	-- Function called when a window is closed
+	---@usage Function called when a window is closed
 	on_close = nil,
-
+	---@usage timeout for notifications in ms, default 5000
+	timeout = 2000,
 	-- Render function for notifications. See notify-render()
 	render = "default",
-
-	-- Default timeout for notifications
-	timeout = 2000,
-
-	-- For stages that change opacity this is treated as the highlight behind the window
-	-- Set this to either a highlight group or an RGB hex value e.g. "#000000"
+	---@usage highlight behind the window for stages that change opacity
 	background_colour = "Normal",
-
-	-- Minimum width for notification windows
+	---@usage minimum width for notification windows
 	minimum_width = 50,
-
 	---@usage notifications with level lower than this would be ignored. [ERROR > WARN > INFO > DEBUG > TRACE]
 	level = "TRACE",
-
-	-- Icons for the different levels
+	---@usage Icons for the different levels
 	icons = {
 		ERROR = icons.diagnostics.Error,
 		WARN = icons.diagnostics.Warning,
