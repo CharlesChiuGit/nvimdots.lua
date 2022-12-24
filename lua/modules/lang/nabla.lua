e35b2325fd69c8f50a2365327d75d187595002c3
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-12-24 15:38:31 +0800

    clean up

--- lua/modules/lang/nabla.lua
+++
@@ -1,3 +0,0 @@
--- nnoremap <leader>p :lua require("nabla").popup()<CR> "
--- Customize with popup({border = ...})  : `single` (default), `double`, `rounded`
-vim.keymap.set("n", "<leader>mp", "<cmd>lua require('nabla').popup()<cr>", { noremap = true, nowait = true })
