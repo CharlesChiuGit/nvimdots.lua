8bc6cbf17e55784a5dc45f67847ff5a580b3b3ae
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-12-25 16:28:58 +0800

    clean up

--- lua/modules/editor/leap.lua
+++ lua/modules/editor/leap.lua
@@ -1,11 +1,2 @@
-vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
-vim.api.nvim_set_hl(0, "LeapMatch", {
-	fg = "white", -- for light themes, set to 'black' or similar
-	bold = true,
-	nocombine = true,
-})
 require("leap").opts.highlight_unlabeled_phase_one_targets = true
-
--- vim.keymap.set("n", "s", "<Nop>", { noremap = false })
--- vim.keymap.set("n", "S", "<Nop>", { noremap = false })
 require("leap").add_default_mappings()
