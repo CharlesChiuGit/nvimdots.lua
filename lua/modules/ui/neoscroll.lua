c857ee79515d6909886b2e960dd3dd0725cfa3e8
CharlesChiuGit <charleschiu2012@gmail.comn>
2022-12-25 17:29:14 +0800

    perf(neoscroll): make scroll faster

--- lua/modules/ui/neoscroll.lua
+++ lua/modules/ui/neoscroll.lua
@@ -1,4 +1,16 @@
 require("neoscroll").setup({
+	-- All these keys will be mapped to their corresponding default scrolling animation
+	mappings = {
+		"<C-u>",
+		"<C-d>",
+		"<C-b>",
+		"<C-f>",
+		"<C-y>",
+		"<C-e>",
+		"zt",
+		"zz",
+		"zb",
+	},
 	hide_cursor = true, -- Hide cursor while scrolling
 	stop_eof = true, -- Stop at <EOF> when scrolling downwards
 	use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
@@ -9,22 +21,3 @@ require("neoscroll").setup({
 	post_hook = nil, -- Function to run after the scrolling animation ends
 	performance_mode = false, -- Options to "Performance Mode" on all buffers.
 })
-
-local t = {}
-
--- Syntax: t[keys] = {function, {function arguments}}
-t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "450" } }
-t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "450" } }
-t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "450" } }
-t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "450" } }
-t["<C-y>"] = { "scroll", { "-0.10", "false", "100" } }
-t["<C-e>"] = { "scroll", { "0.10", "false", "100" } }
-t["zt"] = { "zt", { "250" } }
-t["zz"] = { "zz", { "250" } }
-t["zb"] = { "zb", { "250" } }
-t["<PageUp>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "450" } }
-t["<PageDown>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "450" } }
--- t["<ScrollWheelUp>"] = { "scroll", { "-4", "false", "10" } }
--- t["<ScrollWheelDown>"] = { "scroll", { "4", "false", "10" } }
-
-require("neoscroll.config").set_mappings(t)
