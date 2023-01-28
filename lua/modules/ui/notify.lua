1040757bf04ac6fb339a4596dc75aa7814b20e38
CharlesChiuGit <charleschiu2012@gmail.comn>
2023-01-28 22:37:40 +0800

    chore(notify): update config

--- lua/modules/ui/notify.lua
+++ lua/modules/ui/notify.lua
@@ -13,6 +13,8 @@ require("notify").setup({
 	on_close = nil,
 	---@usage timeout for notifications in ms, default 5000
 	timeout = 2000,
+	-- @usage User render fps value
+	fps = 30,
 	-- Render function for notifications. See notify-render()
 	render = "default",
 	---@usage highlight behind the window for stages that change opacity
