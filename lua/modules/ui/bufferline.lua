0a36192901f1efe191c6758436d725d60a60c99f
CharlesChiuGit <charleschiu2012@gmail.comn>
2023-01-28 23:00:28 +0800

    chore(bufferline): update config

--- lua/modules/ui/bufferline.lua
+++ lua/modules/ui/bufferline.lua
@@ -5,8 +5,8 @@ local opts = {
 	options = {
 		mode = "buffers",
 		numbers = "none",
-		close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
-		right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+		close_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
+		right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
 		left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
 		middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
 
