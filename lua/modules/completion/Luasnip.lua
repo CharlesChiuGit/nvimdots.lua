68d9f58627bc2314fb01326e510858152e9b308e
CharlesChiuGit <charleschiu2012@gmail.com>
2023-02-07 17:58:52 +0800

    rename(snippet): rename `snippets` to `snips`

--- lua/modules/completion/Luasnip.lua
+++ lua/modules/completion/Luasnip.lua
@@ -8,5 +8,5 @@ require("luasnip.loaders.from_lua").lazy_load()
 require("luasnip.loaders.from_snipmate").lazy_load()
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_vscode").lazy_load({
-	paths = { "./snippets/" },
+	paths = { "./snips/" },
 })
