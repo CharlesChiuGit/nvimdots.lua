30c4cdbacd6efbc8478cfb0cedfd3ff23f766884
CharlesChiuGit <charleschiu2012@gmail.com>
2023-02-09 14:35:55 +0800

    style: fix

--- lua/core/global.lua
+++ lua/core/global.lua
@@ -9,7 +9,7 @@ function global:load_variables()
 	self.vim_path = vim.fn.stdpath("config")
 	local path_sep = self.is_windows and "\\" or "/"
 	local home = self.is_windows and os.getenv("USERPROFILE") or os.getenv("HOME")
-    self.cache_dir = home .. path_sep .. ".cache" .. path_sep .. "nvim" .. path_sep
+	self.cache_dir = home .. path_sep .. ".cache" .. path_sep .. "nvim" .. path_sep
 	self.modules_dir = self.vim_path .. path_sep .. "modules"
 	self.home = home
 	self.data_dir = string.format("%s/site/", vim.fn.stdpath("data"))
