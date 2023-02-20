f54b288c413da9a3821df785865395f0d50a87db
CharlesChiuGit <charleschiu2012@gmail.com>
2023-02-20 12:00:57 +0800

    chore(keymap): safe check

--- lua/keymap/helpers.lua
+++ lua/keymap/helpers.lua
@@ -16,8 +16,6 @@ local function not_found_notify(program)
 	vim.notify(string.format("[%s] not found!", program), vim.log.levels.ERROR, { title = "toggleterm.nvim" })
 end
 
-local Terminal = require("toggleterm.terminal").Terminal
-
 local toggleterm_cache = {
 	lazygit = nil,
 	lf = nil,
@@ -47,7 +45,7 @@ function _G.toggle_lf()
 		toggleterm_cache.lf:toggle()
 	else
 		if vim.fn.executable("lf") then
-			toggleterm_cache.lf = Terminal:new({ cmd = "lf", hidden = true, direction = "float" })
+			toggleterm_cache.lf = require("toggleterm.terminal").Terminal:new({ cmd = "lf", hidden = true, direction = "float" })
 			toggleterm_cache.lf:toggle()
 		else
 			not_found_notify("Lf")
@@ -60,7 +58,7 @@ function _G.toggle_htop()
 		toggleterm_cache.htop:toggle()
 	else
 		if vim.fn.executable("htop") then
-			toggleterm_cache.htop = Terminal:new({ cmd = "htop", hidden = true, direction = "float" })
+			toggleterm_cache.htop = require("toggleterm.terminal").Terminal:new({ cmd = "htop", hidden = true, direction = "float" })
 			toggleterm_cache.htop:toggle()
 		else
 			not_found_notify("Htop")
@@ -73,10 +71,10 @@ function _G.toggle_py()
 		toggleterm_cache.python:toggle()
 	else
 		if vim.fn.executable("python3") then
-			toggleterm_cache.python = Terminal:new({ cmd = "python3", hidden = true, direction = "float" })
+			toggleterm_cache.python = require("toggleterm.terminal").Terminal:new({ cmd = "python3", hidden = true, direction = "float" })
 			toggleterm_cache.python:toggle()
 		elseif vim.fn.executable("python") then
-			toggleterm_cache.python = Terminal:new({ cmd = "python", hidden = true, direction = "float" })
+			toggleterm_cache.python = require("toggleterm.terminal").Terminal:new({ cmd = "python", hidden = true, direction = "float" })
 			toggleterm_cache.python:toggle()
 		else
 			not_found_notify("Python")
@@ -89,7 +87,7 @@ function _G.toggle_nvsmi()
 		toggleterm_cache.nvsmi:toggle()
 	else
 		if vim.fn.executable("nvidia-smi") then
-			toggleterm_cache.nvsmi = Terminal:new({ cmd = "watch -n 1 nvidia-smi", hidden = true, direction = "float" })
+			toggleterm_cache.nvsmi = require("toggleterm.terminal").Terminal:new({ cmd = "watch -n 1 nvidia-smi", hidden = true, direction = "float" })
 			toggleterm_cache.nvsmi:toggle()
 		else
 			not_found_notify("nvidia-smi")
