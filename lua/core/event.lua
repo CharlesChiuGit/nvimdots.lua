5583fcdcdc005afbcb94fdfe7ca760946ff5d4e3
CharlesChiuGit <charleschiu2012@gmail.com>
2023-03-06 11:29:26 +0800

    chore(event): center last place

--- lua/core/event.lua
+++ lua/core/event.lua
@@ -57,7 +57,7 @@ vim.api.nvim_create_autocmd("BufRead", {
 	callback = function()
 		vim.api.nvim_create_autocmd("BufWinEnter", {
 			once = true,
-			command = "normal! zx",
+			command = "normal! zz",
 		})
 	end,
 })
