784a6dc9a0e3515bc0383314444ef7d9b2b09a12
CharlesChiuGit <charleschiu2012@gmail.com>
2023-04-12 15:59:29 +0800

    perf(keymap): update keymaps with custom wrapper

--- lua/core/mapping.lua
+++ lua/core/mapping.lua
@@ -1,103 +1,75 @@
--- Shorten function names
-local keymap = vim.keymap.set
-local opts = { noremap = true, silent = true, nowait = true }
+local bind = require("keymap.bind")
+local map_cr = bind.map_cr
+local map_cu = bind.map_cu
+local map_cmd = bind.map_cmd
 
--- Normal mode
--- Source current file
-keymap("n", "<C-o>", "<cmd>so %<cr>", { noremap = true })
--- Save in normal mode
-keymap("n", "<C-s>", "<cmd>write<cr>", { noremap = true })
--- Force save for files need sudo
-keymap("n", "<C-S-s>", "<cmd>execute 'silent! write !sudo tee % >/dev/null' <bar> edit!<cr>", { noremap = true })
--- Increment/decrement
-keymap("n", "+", "<C-a>", opts)
-keymap("n", "-", "<C-x>", opts)
--- Select all
-keymap("n", "<C-a>", "gg<S-v>G", opts)
+local core_map = {
+	-- Suckless
+	["n|<S-Tab>"] = map_cr("normal za"):with_noremap():with_silent():with_desc("edit: Toggle code fold"),
+	["n|<C-s>"] = map_cu("write"):with_noremap():with_silent():with_desc("edit: Save file"),
+	["n|Y"] = map_cmd("y$"):with_desc("edit: Yank text to EOL"),
+	["n|D"] = map_cmd("d$"):with_desc("edit: Delete text to EOL"),
+	["n|n"] = map_cmd("nzzzv"):with_noremap():with_desc("edit: Next search result"),
+	["n|N"] = map_cmd("Nzzzv"):with_noremap():with_desc("edit: Prev search result"),
+	["n|<Esc>"] = map_cmd("<Cmd>noh<CR>"):with_noremap():with_silent():with_desc("edit: Clear search highlight"),
+	-- Do nothing when press "Q", @ThePrimeagen
+	["n|Q"] = map_cmd("<nop>"):with_noremap():with_silent():with_desc("edit: Do nothing when press Q"),
+	-- Keep cursor inplace if below line being append to current line when moving, @ThePrimeagen
+	["n|J"] = map_cmd("mzJ`z"):with_noremap():with_desc("edit: Join next line"),
+	["n|<C-h>"] = map_cmd("<C-w>h"):with_noremap():with_desc("window: Focus left"),
+	["n|<C-l>"] = map_cmd("<C-w>l"):with_noremap():with_desc("window: Focus right"),
+	["n|<C-j>"] = map_cmd("<C-w>j"):with_noremap():with_desc("window: Focus down"),
+	["n|<C-k>"] = map_cmd("<C-w>k"):with_noremap():with_desc("window: Focus up"),
+	["t|<C-h>"] = map_cmd("<Cmd>wincmd h<CR>"):with_silent():with_noremap():with_desc("window: Focus left"),
+	["t|<C-l>"] = map_cmd("<Cmd>wincmd l<CR>"):with_silent():with_noremap():with_desc("window: Focus right"),
+	["t|<C-j>"] = map_cmd("<Cmd>wincmd j<CR>"):with_silent():with_noremap():with_desc("window: Focus down"),
+	["t|<C-k>"] = map_cmd("<Cmd>wincmd k<CR>"):with_silent():with_noremap():with_desc("window: Focus up"),
+	["n|<A-[>"] = map_cr("vertical resize -5"):with_silent():with_desc("window: Resize -5 vertically"),
+	["n|<A-]>"] = map_cr("vertical resize +5"):with_silent():with_desc("window: Resize +5 vertically"),
+	["n|<A-;>"] = map_cr("resize -2"):with_silent():with_desc("window: Resize -2 horizontally"),
+	["n|<A-'>"] = map_cr("resize +2"):with_silent():with_desc("window: Resize +2 horizontally"),
+	["n|<C-q>"] = map_cmd(":wq<CR>"):with_desc("edit: Save file and quit"),
+	["n|<A-S-q>"] = map_cmd(":q!<CR>"):with_desc("edit: Force quit"),
+	["n|<leader>o"] = map_cr("setlocal spell! spelllang=en_us"):with_desc("edit: Toggle spell check"),
+	["n|+"] = map_cmd("<C-a>"):with_noremap():with_silent():with_desc("edit: Increment"),
+	["n|-"] = map_cmd("<C-x>"):with_noremap():with_silent():with_desc("edit: Decrement"),
+	["n|<C-a>"] = map_cmd("gg<S-v>G"):with_noremap():with_silent():with_desc("edit: Select all"),
+	["n|tn"] = map_cmd("<Cmd>tabnew<CR>"):with_noremap():with_silent():with_desc("tab: Create a new tab"),
+	["n|tk"] = map_cmd("<Cmd>tabnext<CR>"):with_noremap():with_silent():with_desc("tab: Move to next tab"),
+	["n|tj"] = map_cmd("<Cmd>tabprevious<CR>"):with_noremap():with_silent():with_desc("tab: Move to previous tab"),
+	["n|to"] = map_cmd("<Cmd>tabonly<CR>"):with_noremap():with_silent():with_desc("tab: Only keep current tab"),
+	-- -- chmod +x current file, @ThePrimeagen
+	["n|<leader><leader>x"] = map_cmd("<Cmd>!chmod +x %<CR>"):with_silent():with_desc("file: chmod +x current file"),
+	-- Insert mode
+	["i|<C-u>"] = map_cmd("<C-G>u<C-U>"):with_noremap():with_desc("edit: Delete previous block"),
+	["i|<C-b>"] = map_cmd("<Left>"):with_noremap():with_desc("edit: Move cursor to left"),
+	["i|<C-a>"] = map_cmd("<ESC>^i"):with_noremap():with_desc("edit: Move cursor to line start"),
+	["i|<C-s>"] = map_cmd("<Esc>:w<CR>"):with_desc("edit: Save file"),
+	["i|<C-q>"] = map_cmd("<Esc>:wq<CR>"):with_desc("edit: Save file and quit"),
+	-- Command mode
+	["c|<C-b>"] = map_cmd("<Left>"):with_noremap():with_desc("edit: Left"),
+	["c|<C-f>"] = map_cmd("<Right>"):with_noremap():with_desc("edit: Right"),
+	["c|<C-a>"] = map_cmd("<Home>"):with_noremap():with_desc("edit: Home"),
+	["c|<C-e>"] = map_cmd("<End>"):with_noremap():with_desc("edit: End"),
+	["c|<C-d>"] = map_cmd("<Del>"):with_noremap():with_desc("edit: Delete"),
+	["c|<C-h>"] = map_cmd("<BS>"):with_noremap():with_desc("edit: Backspace"),
+	["c|<C-t>"] = map_cmd([[<C-R>=expand("%:p:h") . "/" <CR>]])
+		:with_noremap()
+		:with_desc("edit: Complete path of current file"),
+	-- Visual mode
+	-- Move selected lines Up/Down with auto-indent, @ThePrimeagen
+	["v|J"] = map_cmd(":m '>+1<CR>gv=gv"):with_desc("edit: Move this line down"),
+	["v|K"] = map_cmd(":m '<-2<CR>gv=gv"):with_desc("edit: Move this line up"),
+	["v|<"] = map_cmd("<gv"):with_desc("edit: Decrease indent"),
+	["v|>"] = map_cmd(">gv"):with_desc("edit: Increase indent"),
+}
 
---- Buffer keymap (A buffer is in-memory text of a file.)
--- Move one buffer(window) right
-keymap("n", "<C-L>", "<C-W><C-L>", opts)
--- Move one buffer(window) left
-keymap("n", "<C-H>", "<C-W><C-H>", opts)
--- Move one buffer(window) up
-keymap("n", "<C-K>", "<C-W><C-K>", opts)
--- Move one buffer(window) down
-keymap("n", "<C-J>", "<C-W><C-J>", opts)
-
---- Window keymap (A windows is a viewport on a buffer.)
-keymap("n", "<C-j>", "<C-w>j", opts) -- to bottom window
-keymap("n", "<C-k>", "<C-w>k", opts) -- to top window
-keymap("n", "<C-h>", "<C-w>h", opts) -- to left window
-keymap("n", "<C-l>", "<C-w>l", opts) -- to right window
-
---- Tab keymap (A tab page is a collection of windows.)
--- Create a new tab
-keymap("n", "tn", "<cmd>tabnew<cr>", opts)
--- Move to next tab
-keymap("n", "tk", "<cmd>tabnext<cr>", opts)
--- Move to previous tab
-keymap("n", "tj", "<cmd>tabprevious<cr>", opts)
--- Only keep current tab
-keymap("n", "to", "<cmd>tabonly<cr>", opts)
-
--- NOTE: Keep things in paste register after paste it when visual selecting things to replace.
--- useful keymaps to replace text after copying thing, @ThePrimeagen
-keymap("x", "<leader><leader>p", '"_dP', opts)
-keymap("n", "x", '"_x', opts)
-keymap("n", "X", '"_X', opts)
-
--- Move selected lines Up/Down with auto-indent, @ThePrimeagen
-keymap("v", "J", ":m '>+1<cr>gv=gv")
-keymap("v", "K", ":m '<-2<cr>gv=gv")
-
--- Keep cursor inplace if below line being append to current line when moving, @ThePrimeagen
-keymap("n", "J", "mzJ`z")
-
--- Do nothing when press "Q", @ThePrimeagen
-keymap("n", "Q", "<nop>")
+bind.nvim_load_mapping(core_map)
 
 -- start replacment mode of current word, @ThePrimeagen
-keymap("n", "<leader><leader>s", "<cmd>%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
-
--- chmod +x current file, @ThePrimeagen
-keymap("n", "<leader><leader>x", "<cmd>!chmod +x %<cr>")
-
--- Save in insert mode
-keymap("i", "<C-s>", "<esc>:w<cr>", {})
-
--- Blink cursor
-local timer = vim.loop.new_timer()
-local blink = function()
-	local cnt, blink_times = 0, 8
-
-	timer:start(
-		0,
-		100,
-		vim.schedule_wrap(function()
-			vim.cmd("set cursorcolumn! cursorline!")
-
-			cnt = cnt + 1
-			if cnt == blink_times then
-				timer:stop()
-			end
-		end)
-	)
-end
-keymap("n", "<leader>cb", blink)
-
--- Make Ascii art font
-keymap("n", "<leader>aA", "<cmd>.!toilet -w 200 -f standard<cr>", opts)
-keymap("n", "<leader>aa", "<cmd>.!toilet -w 200 -f small<cr>", opts)
--- Make Ascii border
-keymap("n", "<leader>ab", "<cmd>.!toilet -f term -F border<cr>", opts)
--- ┌────┐
--- │test│
--- └────┘
-
--- search and highlight
-keymap("n", "n", "nzzzv", opts)
-keymap("n", "N", "Nzzzv", opts)
-keymap("n", "*", "*zzzv", opts)
-keymap("n", "#", "#zzzv", opts)
-keymap("n", "<esc>", "<cmd>noh<cr>", opts)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader><leader>s",
+	"<cmd>%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
+	{ noremap = true, silent = true, nowait = true, desc = "edit: Start replacment mode of current word" }
+)
