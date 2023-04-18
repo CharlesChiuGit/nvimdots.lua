59fcee460f6c64680328d6eda4faa238f9d53259
CharlesChiuGit <charleschiu@hey.com>
2023-04-18 11:41:30 +0800

    chore(bashls): clean up

--- lua/modules/configs/completion/servers/bashls.lua
+++ lua/modules/configs/completion/servers/bashls.lua
@@ -1,5 +1,2 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/bashls.lua
-return {
-	cmd = { "bash-language-server", "start" },
-	filetypes = { "bash", "sh" },
-}
+return {}
