54c012f6ff8a0d12cfa9aa6849afa9d3c4723969
CharlesChiuGit <charleschiu@hey.com>
2023-04-23 23:12:20 +0800

    perf(pwsh): set shell config for `pwsh` and `windows powersehll` on Windows properly

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -116,15 +116,29 @@ local clipboard_config = function()
 	end
 end
 
-local win_shell_config = function()
+local shell_config = function()
 	if global.is_windows then
-		vim.o.shell = vim.fn.executable("pwsh") and "pwsh" or "powershell"
-		vim.o.shellcmdflag =
-			"-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf-8';"
-		vim.o.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
-		vim.o.shellpipe = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
-		vim.o.shellquote = ""
-		vim.o.shellxquote = ""
+		if not (vim.fn.executable("pwsh") or vim.fn.executable("powershell")) then
+			vim.notify(
+				[[
+Failed to setup terminal config
+PowerShell is either not installed, missing from PATH, or not executable;
+cmd.exe will be used instead for `:!` (shell bang) and toggleterm.nvim.
+You're recommended to install PowerShell for better experience.]],
+				vim.log.levels.WARN,
+				{ title = "[core] Runtime error" }
+			)
+			return
+		end
+
+		local basecmd = "-NoLogo -MTA -ExecutionPolicy RemoteSigned"
+		local ctrlcmd = "-Command [console]::InputEncoding = [console]::OutputEncoding = [System.Text.Encoding]::UTF8"
+		vim.api.nvim_set_option_value("shell", vim.fn.executable("pwsh") and "pwsh" or "powershell", {})
+		vim.api.nvim_set_option_value("shellcmdflag", string.format("%s %s;", basecmd, ctrlcmd), {})
+		vim.api.nvim_set_option_value("shellredir", "-RedirectStandardOutput %s -NoNewWindow -Wait", {})
+		vim.api.nvim_set_option_value("shellpipe", "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode", {})
+		vim.api.nvim_set_option_value("shellquote", nil, {})
+		vim.api.nvim_set_option_value("shellxquote", nil, {})
 	end
 end
 
@@ -134,7 +148,7 @@ local load_core = function()
 	leader_map()
 
 	clipboard_config()
-	win_shell_config()
+	shell_config()
 
 	require("core.options")
 	require("core.mapping")
