e177e37a71119b5d7db525f55eaff603ca00c2ef
CharlesChiuGit <charleschiu@hey.com>
2023-05-29 10:15:31 +0800

    migrate(cmp): adapt to cmp interface change, set `max_view_entries` instead.

--- lua/modules/configs/completion/cmp.lua
+++ lua/modules/configs/completion/cmp.lua
@@ -111,6 +111,10 @@ return function()
 		matching = {
 			disallow_partial_fuzzy_matching = false,
 		},
+		performance = {
+			async_budget = 1,
+			max_view_entries = 300,
+		},
 		-- You can set mappings if you want
 		mapping = cmp.mapping.preset.insert({
 			["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
@@ -149,7 +153,7 @@ return function()
 		},
 		-- You should specify your *installed* sources.
 		sources = {
-			{ name = "nvim_lsp", max_item_count = 350 },
+			{ name = "nvim_lsp" },
 			{ name = "nvim_lua" },
 			{ name = "luasnip" },
 			{ name = "path" },
