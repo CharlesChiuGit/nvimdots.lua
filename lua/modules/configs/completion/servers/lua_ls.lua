445c9647cc677e044d21110bd7dbf7566bba2d34
CharlesChiuGit <charleschiu@hey.com>
2023-07-28 10:49:05 +0800

    fix(lua_ls): prevent `lua_ls` got reattach twice

--- lua/modules/configs/completion/servers/lua_ls.lua
+++ lua/modules/configs/completion/servers/lua_ls.lua
@@ -1,24 +1,9 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/server_configurations/lua_ls.lua
 
 return {
+	cmd = { "lua-language-server" },
 	settings = {
 		Lua = {
-			runtime = {
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				enable = true,
-				globals = { "vim" },
-				disable = { "different-requires" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-				},
-				maxPreload = 2000,
-				preloadFileSize = 1000,
-			},
 			hint = { enable = true, setType = true },
 			format = { enable = false },
 			telemetry = { enable = false },
