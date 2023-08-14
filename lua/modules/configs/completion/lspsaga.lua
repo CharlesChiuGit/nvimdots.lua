9c25925a1fc96c862d564aba0e252958c5c22832
CharlesChiuGit <charleschiu@hey.com>
2023-08-14 14:45:35 +0800

    feat(lspsaga): make codeaction effective linewise

--- lua/modules/configs/completion/lspsaga.lua
+++ lua/modules/configs/completion/lspsaga.lua
@@ -1,84 +1,183 @@
 return function()
-	local icons = { ui = require("modules.utils.icons").get("ui", true) }
-	local glance = require("glance")
-	local actions = glance.actions
+	require("modules.utils").gen_lspkind_hl()
 
-	glance.setup({
-		height = 20,
-		zindex = 50,
-		preview_win_opts = {
-			cursorline = true,
-			number = true,
-			wrap = true,
+	local icons = {
+		cmp = require("modules.utils.icons").get("cmp", true),
+		diagnostics = require("modules.utils.icons").get("diagnostics", true),
+		kind = require("modules.utils.icons").get("kind", true),
+		type = require("modules.utils.icons").get("type", true),
+		ui = require("modules.utils.icons").get("ui", true),
+	}
+
+	local function set_sidebar_icons()
+		-- Set icons for sidebar.
+		local diagnostic_icons = {
+			Error = icons.diagnostics.Error_alt,
+			Warn = icons.diagnostics.Warning_alt,
+			Info = icons.diagnostics.Information_alt,
+			Hint = icons.diagnostics.Hint_alt,
+		}
+		for type, icon in pairs(diagnostic_icons) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl })
+		end
+	end
+
+	set_sidebar_icons()
+
+	require("lspsaga").setup({
+		-- Breadcrumbs: https://dev.neovim.pro/lspsaga/breadcrumbs/
+		symbol_in_winbar = {
+			enable = true,
+			separator = " " .. icons.ui.DoubleSeparator,
+			hide_keyword = false,
+			show_file = false,
+			folder_level = 1,
+			color_mode = true,
+			delay = 100,
 		},
-		border = {
-			enable = require("core.settings").transparent_background,
-			top_char = "―",
-			bottom_char = "―",
-		},
-		list = {
-			position = "right",
-			width = 0.33, -- 33% width relative to the active window, min 0.1, max 0.5
-		},
-		folds = {
-			folded = true, -- Automatically fold list on startup
-			fold_closed = icons.ui.ArrowClosed,
-			fold_open = icons.ui.ArrowOpen,
-		},
-		indent_lines = { enable = true },
-		winbar = { enable = true },
-		mappings = {
-			list = {
-				["k"] = actions.previous,
-				["j"] = actions.next,
-				["<Up>"] = actions.previous,
-				["<Down>"] = actions.next,
-				["<S-Tab>"] = actions.previous_location, -- Bring the cursor to the previous location skipping groups in the list
-				["<Tab>"] = actions.next_location, -- Bring the cursor to the next location skipping groups in the list
-				["<C-u>"] = actions.preview_scroll_win(8),
-				["<C-d>"] = actions.preview_scroll_win(-8),
-				["<CR>"] = actions.jump,
-				["v"] = actions.jump_vsplit,
-				["s"] = actions.jump_split,
-				["t"] = actions.jump_tab,
-				["c"] = actions.close_fold,
-				["o"] = actions.open_fold,
-				["[]"] = actions.enter_win("preview"), -- Focus preview window
-				["q"] = actions.close,
-				["Q"] = actions.close,
-				["<Esc>"] = actions.close,
-				["gq"] = actions.quickfix,
-			},
-			preview = {
-				["Q"] = actions.close,
-				["<C-c>q"] = actions.close,
-				["<C-c>o"] = actions.jump,
-				["<C-c>v"] = actions.jump_vsplit,
-				["<C-c>s"] = actions.jump_split,
-				["<C-c>t"] = actions.jump_tab,
-				["<C-p>"] = actions.previous_location,
-				["<C-n>"] = actions.next_location,
-				["[]"] = actions.enter_win("list"), -- Focus list window
+		-- https://dev.neovim.pro/lspsaga/callhierarchy/
+		callhierarchy = {
+			layout = "float",
+			keys = {
+				edit = "e",
+				vsplit = "v",
+				split = "s",
+				tabe = "t",
+				quit = "q",
+				shuttle = "[]",
+				toggle_or_req = "u",
+				close = "<Esc>",
 			},
 		},
-		hooks = {
-			before_open = function(results, open, _, method)
-				if #results == 0 then
-					vim.notify(
-						"This method is not supported by any of the servers registered for the current buffer",
-						vim.log.levels.WARN,
-						{ title = "Glance" }
-					)
-				elseif #results == 1 and method == "references" then
-					vim.notify(
-						"The identifier under cursor is the only one found",
-						vim.log.levels.INFO,
-						{ title = "Glance" }
-					)
-				else
-					open(results)
-				end
-			end,
+		-- https://dev.neovim.pro/lspsaga/codeaction/
+		code_action = {
+			num_shortcut = true,
+			only_in_cursor = false,
+			show_server_name = true,
+			extend_gitsigns = false,
+			keys = {
+				quit = "q",
+				exec = "<CR>",
+			},
 		},
+		-- https://dev.neovim.pro/lspsaga/diagnostic/
+		diagnostic = {
+			show_code_action = true,
+			jump_num_shortcut = true,
+			max_width = 0.5,
+			max_height = 0.6,
+			text_hl_follow = true,
+			border_follow = true,
+			extend_relatedInformation = true,
+			show_layout = "float",
+			show_normal_height = 10,
+			max_show_width = 0.9,
+			max_show_height = 0.6,
+			diagnostic_only_current = false,
+			keys = {
+				exec_action = "r",
+				quit = "q",
+				toggle_or_jump = "<CR>",
+				quit_in_show = { "q", "<Esc>" },
+			},
+		},
+		-- https://dev.neovim.pro/lspsaga/hover/
+		hover = {
+			max_width = 0.45,
+			max_height = 0.7,
+			open_link = "gl",
+			open_cmd = "silent !" .. require("core.settings").external_browser,
+		},
+		-- https://dev.neovim.pro/lspsaga/implement/
+		implement = {
+			enable = true,
+			sign = true,
+			virtual_text = false,
+			priority = 100,
+		},
+		-- https://dev.neovim.pro/lspsaga/lightbulb/
+		lightbulb = {
+			enable = false,
+			sign = true,
+			virtual_text = false,
+			debounce = 10,
+			sign_priority = 20,
+		},
+		-- https://dev.neovim.pro/lspsaga/rename/
+		rename = {
+			in_select = false,
+			auto_save = false,
+			project_max_width = 0.5,
+			project_max_height = 0.5,
+			keys = {
+				quit = "<C-c>",
+				exec = "<CR>",
+				select = "x",
+			},
+		},
+		-- https://dev.neovim.pro/lspsaga/misc/#beacon
+		beacon = {
+			enable = true,
+			frequency = 12,
+		},
+		-- https://dev.neovim.pro/lspsaga/misc/#generic-ui-options
+		ui = {
+			border = "single", -- Can be single, double, rounded, solid, shadow.
+			devicon = true,
+			title = true,
+			expand = icons.ui.ArrowClosed,
+			collapse = icons.ui.ArrowOpen,
+			code_action = icons.ui.CodeAction,
+			actionfix = icons.ui.Spell,
+			lines = { "┗", "┣", "┃", "━", "┏" },
+			imp_sign = icons.kind.Implementation,
+			kind = {
+				-- Kind
+				Class = { icons.kind.Class, "LspKindClass" },
+				Constant = { icons.kind.Constant, "LspKindConstant" },
+				Constructor = { icons.kind.Constructor, "LspKindConstructor" },
+				Enum = { icons.kind.Enum, "LspKindEnum" },
+				EnumMember = { icons.kind.EnumMember, "LspKindEnumMember" },
+				Event = { icons.kind.Event, "LspKindEvent" },
+				Field = { icons.kind.Field, "LspKindField" },
+				File = { icons.kind.File, "LspKindFile" },
+				Function = { icons.kind.Function, "LspKindFunction" },
+				Interface = { icons.kind.Interface, "LspKindInterface" },
+				Key = { icons.kind.Keyword, "LspKindKey" },
+				Method = { icons.kind.Method, "LspKindMethod" },
+				Module = { icons.kind.Module, "LspKindModule" },
+				Namespace = { icons.kind.Namespace, "LspKindNamespace" },
+				Number = { icons.kind.Number, "LspKindNumber" },
+				Operator = { icons.kind.Operator, "LspKindOperator" },
+				Package = { icons.kind.Package, "LspKindPackage" },
+				Property = { icons.kind.Property, "LspKindProperty" },
+				Struct = { icons.kind.Struct, "LspKindStruct" },
+				TypeParameter = { icons.kind.TypeParameter, "LspKindTypeParameter" },
+				Variable = { icons.kind.Variable, "LspKindVariable" },
+				-- Type
+				Array = { icons.type.Array, "LspKindArray" },
+				Boolean = { icons.type.Boolean, "LspKindBoolean" },
+				Null = { icons.type.Null, "LspKindNull" },
+				Object = { icons.type.Object, "LspKindObject" },
+				String = { icons.type.String, "LspKindString" },
+				-- ccls-specific icons.
+				TypeAlias = { icons.kind.TypeAlias, "LspKindTypeAlias" },
+				Parameter = { icons.kind.Parameter, "LspKindParameter" },
+				StaticMethod = { icons.kind.StaticMethod, "LspKindStaticMethod" },
+				-- Microsoft-specific icons.
+				Text = { icons.kind.Text, "LspKindText" },
+				Snippet = { icons.kind.Snippet, "LspKindSnippet" },
+				Folder = { icons.kind.Folder, "LspKindFolder" },
+				Unit = { icons.kind.Unit, "LspKindUnit" },
+				Value = { icons.kind.Value, "LspKindValue" },
+			},
+		},
+		-- https://dev.neovim.pro/lspsaga/misc/#scrolling-keymaps
+		scroll_preview = {
+			scroll_down = "<C-d>",
+			scroll_up = "<C-u>",
+		},
+		request_timeout = 3000,
 	})
 end
