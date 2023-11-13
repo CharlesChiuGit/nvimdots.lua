82ea6b55eb7d2521a05d453b68864c62ee007d54
CharlesChiuGit <charleschiu@hey.com>
2023-11-13 11:21:42 +0800

    feat(treesitter): use ts-indent and disable legacy syntax engine

--- lua/modules/configs/editor/treesitter.lua
+++ lua/modules/configs/editor/treesitter.lua
@@ -27,10 +27,7 @@ return vim.schedule_wrap(function()
 				local ok, is_large_file = pcall(vim.api.nvim_buf_get_var, bufnr, "bigfile_disable_treesitter")
 				return ok and is_large_file
 			end,
-			additional_vim_regex_highlighting = { "c", "cpp" },
-		},
-		indent = {
-			enable = false,
+			additional_vim_regex_highlighting = false,
 		},
 		autotag = {
 			enable = true,
@@ -71,6 +68,7 @@ return vim.schedule_wrap(function()
 			enable = true,
 			enable_autocmd = false,
 		},
+		indent = { enable = true },
 		matchup = { enable = true },
 	}, false, require("nvim-treesitter.configs").setup)
 
