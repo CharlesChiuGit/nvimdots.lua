be64718bb78c12c1a3f90e323dba8bdf28ee1a8d
CharlesChiuGit <charleschiu@hey.com>
2023-11-20 16:55:34 +0800

    feat(fidget)!: support v2

--- lua/modules/configs/ui/fidget.lua
+++ lua/modules/configs/ui/fidget.lua
@@ -4,49 +4,24 @@ return function()
 	}
 
 	require("modules.utils").load_plugin("fidget", {
-		text = {
-			spinner = "pipe", -- animation shown when tasks are ongoing
-			done = icons.ui.Check, -- character shown when all tasks are complete
-			commenced = "Started", -- message shown when task starts
-			completed = "Completed", -- message shown when task completes
+		progress = {
+			suppress_on_insert = false, -- Suppress new messages while in insert mode
+			ignore_done_already = false, -- Ignore new tasks that are already complete
+			ignore = { "null-ls" }, -- List of LSP servers to ignore
+			display = {
+				render_limit = 5, -- How many LSP messages to show at once
+				done_ttl = 2, -- How long a message should persist after completion
+				done_icon = icons.ui.Accepted, -- Icon shown when all LSP progress tasks are complete
+				done_style = "Constant", -- Highlight group for completed LSP tasks
+			},
 		},
-		align = {
-			bottom = true, -- align fidgets along bottom edge of buffer
-			right = true, -- align fidgets along right edge of buffer
-		},
-		timer = {
-			spinner_rate = 125, -- frame rate of spinner animation, in ms
-			fidget_decay = 2000, -- how long to keep around empty fidget, in ms
-			task_decay = 1000, -- how long to keep around completed task, in ms
-		},
-		window = {
-			blend = 0, -- catppuccin integration
-		},
-		fmt = {
-			leftpad = true, -- right-justify text in fidget box
-			stack_upwards = true, -- list of tasks grows upwards
-			max_messages = 3, -- The maximum number of messages stacked at any give time
-			max_width = 0, -- maximum width of the fidget box
-			-- function to format fidget title
-			fidget = function(fidget_name, spinner)
-				return string.format("%s %s", spinner, fidget_name)
-			end,
-			-- function to format each task line
-			task = function(task_name, message, percentage)
-				return string.format(
-					"%s%s [%s]",
-					message,
-					percentage and string.format(" (%s%%)", percentage) or "",
-					task_name
-				)
-			end,
-		},
-		debug = {
-			logging = false, -- whether to enable logging, for debugging
-			strict = false, -- whether to interpret LSP strictly
-		},
-		sources = {
-			["null-ls"] = { ignore = true },
+		-- Options related to notification subsystem
+		notification = {
+			override_vim_notify = false, -- Automatically override vim.notify() with Fidget
+			window = {
+				winblend = 0, -- Background color opacity in the notification window
+				zindex = 75, -- Stacking priority of the notification window
+			},
 		},
 	})
 end
