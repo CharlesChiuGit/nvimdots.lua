46a7f0f2846c4e8b765345d31110e7a206eed45c
CharlesChiuGit <charleschiu@hey.com>
2024-02-01 09:58:53 +0800

    feat(neodev): add it back

--- lua/modules/plugins/completion.lua
+++ lua/modules/plugins/completion.lua
@@ -102,6 +102,10 @@ end
 
 -- Adding *nvim config dir*, *nvim runtime dir*, *all plugin dir(with /lua dir)* to get
 -- hover docs and function signatures, but it takes too much time to load all dirs, use it if needed.
--- completion["folke/neodev.nvim"] = {}
+completion["folke/neodev.nvim"] = {
+	lazy = true,
+	event = "LspAttach",
+	ft = "lua",
+}
 
 return completion
