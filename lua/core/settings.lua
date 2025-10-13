962c08444fbee92cfd60eb71ab0921ea86ab06ed
CharlesChiuGit <charleschiu2012@gmail.com>
2025-10-13 11:09:21 +0800

    feat(settings): wait longer for formatting

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -14,7 +14,7 @@ settings["format_on_save"] = true
 
 -- Format timeout in milliseconds.
 ---@type number
-settings["format_timeout"] = 1000
+settings["format_timeout"] = 3000
 
 -- Set to false to disable format notification.
 ---@type boolean
